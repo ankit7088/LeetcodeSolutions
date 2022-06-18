2a3af34b5991f04612e094a6a9db14b4f0f794b3
Ankit Kumar <0404ankitkumar@gmail.com>
2022-06-18 17:27:52 +0530

    1446. Consecutive Characters || 461. Hamming Distance || Leetcode

---
+++ 1446. Consecutive Characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxPower(string s) {
+    if(s.size()==0)
+    return 0;
+    int ans=1,count=1;
+    for(int i=1;i<s.size();i++){
+        if(s[i]==s[i-1])
+        {
+            count++;
+            ans=max(ans,count);
+        }
+        else
+        count=1;
+    }
+    return ans;
+    }
+};
